36a2d64e70c90c1d15b68d2d6bf60c82c8270627
imse <imse30@gmail.com>
2020-01-27 00:15:20 +0300

    pre final

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ let navButton = document.querySelector('.navbar-toggler');
 navButton.addEventListener('click', function() {
     title.classList.toggle('top');
 })
-//BURGER MENU end
+//BURGER MENU
 
 //SLIDER
 
@@ -111,42 +111,3 @@ function moveToSlide(n) {
   currentSlide = (n + slide.length) % slide.length;
   slide[currentSlide].className = 'slide active'; 
 }
-
-
-
-
-
-
-
-
-
-// buttonRight.addEventListener('click', function(){
-//   slide.forEach(function callback(elem, index) {
-//     let lastImg = slide[slide.length - 1].childNodes[0];
-//     if (index < slide.length - 1) {
-//       let image = elem.childNodes[0];
-//       image.remove();
-//       slide[index + 1].append(image);
-//     }
-//     else if (index == slide.length - 1) {
-//       slide[0].append(lastImg);
-//     }
-//   });
-// })
-
-// buttonLeft.addEventListener('click', function(){
-//   slide.forEach(function callback(elem, index) {
-//     let firstImg = slide[0].childNodes[0];
-//     let image = elem.childNodes[0];
-//      slide[2];
-//     if (index >= 0) {
-//       image.remove();
-//       slide[slide.length - 1].append(firstImg);
-//       slide[index - 1].append(image);
-//     }
-//     // else if (index = 0) {
-//     //   slide[slide.length - (slide.length / 2)].append(firstImg);
-//     // }
-    
-//   });
-// })
